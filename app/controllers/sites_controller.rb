d8de11626582bf5ee06ac7fbeee5393a14623282
Jen Delamere <jen@gina.alaska.edu>
2014-01-30 09:40:51 -0900

    Add sites controller.

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,8 @@
+class SitesController < ApplicationController
+  def show
+    
+    respond_to do |format|
+      format.js
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ImiqMap::Application.routes.draw do
   resources :maps
-
+  resources :sites, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
