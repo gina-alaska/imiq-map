613dddc56fe0895e1f68ee4709bd691e98e4c974
Will Fisher <will@teknofire.net>
2014-03-07 17:21:29 -0900

    make sure we don't iterate when variables is nil

--- app/models/export.rb
+++ app/models/export.rb
@@ -8,10 +8,12 @@ class Export < ActiveRecord::Base
     opts[:startdate] = self.starts_at if self.starts_at?
     opts[:enddate] = self.ends_at if self.ends_at?
 
-    self.variables.each do |var|
-      @urls << ImiqAPI.export_uri(self.sites, self.timestep, var, opts)
+    unless self.variables.nil?
+      self.variables.each do |var|
+        @urls << ImiqAPI.export_uri(self.sites, self.timestep, var, opts)
+      end
     end
-
+    
     @urls
   end
 end
