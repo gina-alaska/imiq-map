910b6be4f2b48a71b5b68dc88036a6bac536310a
Will Fisher <will@teknofire.net>
2014-05-05 17:33:58 -0800

    adding unicorn to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,10 +16,11 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets solr vendor/bundle p
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
+set :chruby_ruby, 'ruby-2.1.1'
+
 namespace :deploy do
   desc 'Restart application'
   task :restart do
-    puts "We currently don't have support for auto restarting, please login to browse-web0 and run 'sudo service unicorn_browser_ng restart'"
     on roles(:web), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
@@ -27,8 +28,19 @@ namespace :deploy do
       #   execute :rake, 'sunspot:solr:stop'
       #   execute :rake, 'sunspot:solr:start'
       # end
-      execute 'sudo service unicorn_imiq_map stop'
-      execute 'sudo service unicorn_imiq_map start'
+      execute 'TERM=dumb sudo service unicorn_imiq_map stop'
+      execute 'TERM=dumb sudo service unicorn_imiq_map start'
+    end
+  end
+  task :start do
+    on roles(:web), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      # within release_path do
+      #   execute :rake, 'sunspot:solr:stop'
+      #   execute :rake, 'sunspot:solr:start'
+      # end
+      execute 'TERM=dumb sudo service unicorn_imiq_map start'
     end
   end
 
