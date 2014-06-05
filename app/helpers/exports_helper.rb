e2b1be66029682d2329ebdec5f8f8f291543d572
Will Fisher <will@teknofire.net>
2014-06-04 16:35:28 -0800

    trying to simplify the export form views

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -1,2 +1,15 @@
 module ExportsHelper
+  def params_list(site)
+    list = []
+    
+    if site['derived_variables']['daily'].size > 0
+      list += site['derived_variables']['daily'].map { |v| v[0] }
+    end
+      
+    if site['derived_variables']['hourly'].size > 0
+      list += site['derived_variables']['hourly'].map { |v| v[0] }
+    end
+      
+    list.try(:join, ', ')
+  end
 end
