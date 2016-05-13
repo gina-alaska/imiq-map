0fa37e547676fc9f66d869b1d52a2826138a78b2
Will Fisher <will@gina.alaska.edu>
2016-05-13 12:16:55 -0800

    adding basic test to try and verify the user permissions

--- test/controllers/exports_controller_test.rb
+++ test/controllers/exports_controller_test.rb
@@ -4,4 +4,12 @@ class ExportsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "cannot read other users export" do
+    login_user(:one)
+
+    get :show, id: exports(:one)
+
+    assert_redirected_to root_url
+  end
 end
