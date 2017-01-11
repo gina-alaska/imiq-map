5e03d4e671b9b80153313d65c1533142d1eef1e0
grimm <jason@gina.alaska.edu>
2017-01-10 16:35:11 -0900

    Added new admin boolean field to user table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003203051) do
+ActiveRecord::Schema.define(version: 20170111013134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,8 +114,9 @@ ActiveRecord::Schema.define(version: 20161003203051) do
     t.string   "name"
     t.string   "email"
     t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "admin",      default: false
   end
 
   add_foreign_key "bounds", "searches"
