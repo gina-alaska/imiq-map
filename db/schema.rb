b4c4090b00ab7fd31995ca90e57724bf1ce69fbc
Rawser Spicer <rwspicer@alaska.edu>
2017-10-24 12:45:57 -0800

    loading page will trigger all missing downloads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111013134) do
+ActiveRecord::Schema.define(version: 20170920050406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170111013134) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "authorizations", force: :cascade do |t|
@@ -78,6 +84,13 @@ ActiveRecord::Schema.define(version: 20170111013134) do
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
